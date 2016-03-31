7a16c64ffac9180b88d5296a427d88f996e2cd86
Emilio J Grande <emilio@jsjutsu.com>
2016-03-31 19:12:56 +0200

    Add routing

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -5,6 +5,23 @@
         .module('app', [
           'ui.bootstrap',
           'ngResource',
-          'ngMessages'
+          'ngMessages',
+          'ui.router'
         ])
+        .config(configure)
+
+  function configure ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('list', {
+        url: '/list',
+        templateUrl: 'templates/list.html'
+      })
+      .state('addWorker', {
+        url: '/addworker',
+        templateUrl: 'templates/add-worker.html'
+      })
+    $urlRouterProvider.otherwise('/list')
+  }
+
+  configure.$inject = ['$stateProvider', '$urlRouterProvider']
 }())
