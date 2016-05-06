53dba99dc82b0b22d60d3c13196043edeb49e30a
Emilio J Grande <emilio@jsjutsu.com>
2016-05-06 14:29:10 +0200

    Add authentication

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -7,17 +7,19 @@
           'ngResource',
           'ngMessages',
           'ui.router',
-          'angular-loading-bar'
+          'angular-loading-bar',
+          'satellizer'
         ])
         .config(configure)
 
-  function configure ($stateProvider, $urlRouterProvider) {
+  function configure ($stateProvider, $urlRouterProvider, $authProvider) {
     $stateProvider
       .state('list', {
         url: '/list',
         templateUrl: 'templates/list.html',
         controller: 'ListController as vm',
         resolve: {
+          loginRequired: loginRequired,
           initialListResponse: function (workers) {
             return workers.list(1)
           }
@@ -26,20 +28,45 @@
       .state('addWorker', {
         url: '/addworker',
         templateUrl: 'templates/add-worker.html',
-        controller: 'NewWorkerController as vm'
+        controller: 'NewWorkerController as vm',
+        resolve: {
+          loginRequired: loginRequired
+        }
       })
       .state('viewWorker', {
         url: '/viewworker/:id',
         templateUrl: 'templates/view-worker.html',
         controller: 'ViewWorkerController as vm',
         resolve: {
+          loginRequired: loginRequired,
           workerDataResponse: function (workers, $stateParams) {
             return workers.get($stateParams.id)
           }
         }
       })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'LoginController as vm'
+      })
     $urlRouterProvider.otherwise('/list')
+
+    // Auth configuration
+    $authProvider.baseUrl = 'http://localhost:3000'
+    $authProvider.loginUrl = '/login'
+    $authProvider.tokenName = 'token'
   }
 
-  configure.$inject = ['$stateProvider', '$urlRouterProvider']
+  function loginRequired ($q, $auth, $location) {
+    var deferred = $q.defer()
+    if ($auth.isAuthenticated()) {
+      deferred.resolve()
+    } else {
+      $location.path('/login')
+    }
+    return deferred.promise
+  }
+
+  configure.$inject = ['$stateProvider', '$urlRouterProvider', '$authProvider']
+  loginRequired.$inject = ['$q', '$auth', '$location']
 }())
